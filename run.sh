1f5501e6f93704923a053e81a799de6037c980f9
vibrioh <vibrioh@gmail.com>
2017-11-07 17:07:01 -0500

    run samples

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python iterative_set_expansion.py AIzaSyA_h1kXl0JIC4d2RAkeq-VRWMTbVOijrwA 015239954879085458485:jvjbixbxjiu 4 0.35 "bill gates microsoft" 10
+
+python iterative_set_expansion.py AIzaSyA_h1kXl0JIC4d2RAkeq-VRWMTbVOijrwA 015239954879085458485:jvjbixbxjiu 3 0.15 "microsoft europe"  10
+
+python iterative_set_expansion.py AIzaSyA_h1kXl0JIC4d2RAkeq-VRWMTbVOijrwA 015239954879085458485:jvjbixbxjiu 2 0.2 "Columbia University New York City" 10
